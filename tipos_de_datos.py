c5cb1b30bf0a53c8f66a679dc1ba9ab41861fcdd
Shamanesss <mentulbenodorna@gmail.com>
2026-02-28 18:21:11 +0100

    added the second folder

---
+++ tipos_de_datos.py
@@ -0,0 +1,17 @@
+#Boleans
+#Es un valor verdadero o falso
+
+is_active = False
+
+print(is_active)
+
+#Number - almacena numeros
+
+age = 34
+prize = 19.99
+number = 2 + 3j
+
+print(age, prize, number)
+
+
+#String - almacena cualquier tipo de cadena de caracteres
